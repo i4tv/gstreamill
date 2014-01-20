a2c23d1456d1a0beaa4cfea02502bcc988aa9bc0
Zhang Ping <dqzhangp@163.com>
2014-01-20 21:19:34 +0800

    mv job livejob

--- src/livejob.h
+++ src/livejob.h
@@ -9,6 +9,8 @@
 
 #include "job.h"
 
+guint64 livejob_encoder_output_gop_size (EncoderOutput *encoder_output, guint64 rap_addr);
+guint64 livejob_encoder_output_rap_next (EncoderOutput *encoder_output, guint64 rap_addr);
 gchar * livejob_get_master_m3u8_playlist (LiveJob *livejob);
 
 #endif /* __LIVEJOB_H__ */
