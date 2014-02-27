62282a0919c33d0bc935fbe0f7188a29ade701dd
Zhang Ping <dqzhangp@163.com>
2014-02-27 16:29:04 +0800

    m3u8 test

--- test/m3u8client.py
+++ test/m3u8client.py
@@ -4,9 +4,35 @@ pip install m3u8 first, please
 """
 
 import m3u8
+import time
 
-m3u8_c = m3u8.load("http://192.168.7.90:20119/live/cctv1/encoder/0/playlist.m3u8")
+url = "http://192.168.7.90:20119/live/cctv1/encoder/0/playlist.m3u8"
+media_sequence = 0
 
-print m3u8_c.target_duration
-print m3u8_c.media_sequence
-print m3u8_c.segments
+playlist = m3u8.load(url)
+current_sequence = media_sequence = playlist.media_sequence
+for segment in playlist.segments:
+    print current_sequence, segment.uri
+    current_sequence += 1
+
+while True:
+    playlist = m3u8.load(url)
+    if playlist.media_sequence == media_sequence:
+        time.sleep(1)
+        continue
+    media_sequence = playlist.media_sequence
+    target_duration = playlist.target_duration
+
+    if current_sequence < media_sequence:
+        print "ERROR : missing segment"
+        current_sequence = media_sequence
+
+    index = -1
+    for segment in playlist.segments:
+        index += 1
+        if media_sequence + index < current_sequence:
+            continue
+        print current_sequence, segment.uri
+        current_sequence += 1
+
+    time.sleep(target_duration - 1)
