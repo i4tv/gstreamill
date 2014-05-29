e5240365c7c0f611c39274388e3f850000195bc2
Zhang Ping <dqzhangp@163.com>
2014-05-30 00:51:18 +0800

    postjob tools bug

--- tools/postjob.py
+++ tools/postjob.py
@@ -23,6 +23,10 @@ for movie in movies:
     while len(status["job"]) >= 5: # 当前运行的job数达到了4个，等待10s
         print "%d job, waitting ..." % len(status["job"])
         time.sleep(10)
+        gstreamill = httplib.HTTPConnection("localhost:20118")
+        gstreamill.request("GET", "/stat/gstreamill")
+        response = gstreamill.getresponse()
+        status = json.loads(response.read())
         continue
 
     # 当前运行的job数少于4个，提交一个job
