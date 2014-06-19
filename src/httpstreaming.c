2734e6fe628aceaf764de86904e5c45ea824641e
Zhang Ping <dqzhangp@163.com>
2014-06-19 11:44:17 +0800

    replace httpserver_write work

--- src/httpstreaming.c
+++ src/httpstreaming.c
@@ -465,7 +465,9 @@ static GstClockTime http_request_process (HTTPStreaming *httpstreaming, RequestD
                 priv_data->encoder_output = encoder_output;
                 priv_data->rap_addr = *(encoder_output->last_rap_addr);
                 request_data->priv_data = priv_data;
-                http_progress_play_priv_data_init (httpstreaming, request_data, priv_data);
+                if (is_http_progress_play_request) {
+                        http_progress_play_priv_data_init (httpstreaming, request_data, priv_data);
+                }
                 return ret > 0? 10 * GST_MSECOND + g_random_int_range (1, 1000000) : GST_CLOCK_TIME_NONE;
 
         } else if (ret == -1) {
