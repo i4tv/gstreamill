fa8f441bed22588612c9c043577c010729cdbc53
Zhang Ping <dqzhangp@163.com>
2014-09-25 22:47:05 +0800

    m3u8 transcode process

--- src/job.c
+++ src/job.c
@@ -236,10 +236,8 @@ static gchar * render_master_m3u8_playlist (Job *job)
         for (i = 0; i < job->output->encoder_count; i++) {
                 p = g_strdup_printf ("encoder.%d.elements.x264enc.property.bitrate", i);
                 value = jobdesc_element_property_value (job->description, p);
-                if (value == NULL) {
-                        GST_ERROR ("job %s's property %s not found", job->name, p);
-
-                } else {
+                if (value != NULL) {
+                        GST_INFO ("job %s with m3u8 output, append end tag", job->name);
                         g_string_append_printf (master_m3u8_playlist, M3U8_STREAM_INF_TAG, 1, value);
                         g_string_append_printf (master_m3u8_playlist, "encoder/%d/playlist.m3u8\n", i);
                         g_free (value);
