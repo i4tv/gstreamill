6fb5f6decb402e11f44b89b4128270257ddd0c71
Zhang Ping <dqzhangp@163.com>
2015-01-29 06:03:37 +0000

    autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common!"
-    exit 1
-}
-
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
+aclocal
+libtoolize --force
+automake --foreign --add-missing --copy
+autoconf
+autoheader
