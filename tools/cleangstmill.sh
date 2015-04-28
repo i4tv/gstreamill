5738962667f07e56c0c332375f5f37810af546c5
Zhang Ping <dqzhangp@163.com>
2015-04-28 21:49:30 +0800

    mqueue is obsolete

--- tools/cleangstmill.sh
+++ tools/cleangstmill.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
 rm -rf /var/run/gstreamill/gstreamill.pid
-mount -t mqueue none /var/run/gstreamill/mq
-mkdir -p /var/run/gstreamill/mq
-rm -rf /var/run/gstreamill/mq/*
-umount /var/run/gstreamill/mq
 rm -rf /dev/shm/*
