64c21655599a42c0741943981c58f9d24d8adae8
Zhang Ping <dqzhangp@163.com>
2015-07-08 08:47:08 +0800

    remove unused code in tssegment.h

--- src/tssegment.h
+++ src/tssegment.h
@@ -261,115 +261,11 @@ typedef struct _TSStream
         GstMpegTsPMTStream *stream;
 } TSStream;
 
-/*
- * PES stream_id_extension assignments (if stream_id == ID_EXTENDED_STREAM_ID)
- *
- *  000 0000             IPMP Control Information stream
- *  000 0001             IPMP Stream
- *  000 0010 - 001 0001  ISO/IEC 14496-17 text Streams
- *  001 0010 - 010 0001  ISO/IEC 23002-3 auxiliary video data Streams
- *  ... .... - 011 1111  Reserved
- *
- *  PRIVATE STREAM RANGES (But known as used)
- *  101 0101 - 101 1111  VC-1
- *  110 0000 - 110 1111  Dirac (VC-1)
- *
- *  111 0001             AC3 or independent sub-stream 0 of EAC3/DD+
- *                       DTS or core sub-stream
- *  111 0010             dependent sub-stream of EAC3/DD+
- *                       DTS extension sub-stream
- *                       Secondary EAC3/DD+
- *                       Secondary DTS-HD LBR
- *  111 0110             AC3 in MLP/TrueHD
- *  1xx xxxx    private_stream
- */
-#define EXT_ID_IPMP_CONTORL_INFORMATION_STREAM  0x00
-#define EXT_ID_IPMP_STREAM                      0x01
-
-/* VC-1 */
-#define EXT_ID_VC1_FIRST                        0x55
-#define EXT_ID_VC1_LAST                         0x5F
-
-typedef enum {
-  PES_FLAG_PRIORITY             = 1 << 3,       /* PES_priority (present: high-priority) */
-  PES_FLAG_DATA_ALIGNMENT       = 1 << 2,       /* data_alignment_indicator */
-  PES_FLAG_COPYRIGHT            = 1 << 1,       /* copyright */
-  PES_FLAG_ORIGINAL_OR_COPY     = 1 << 0        /* original_or_copy */
-} PESHeaderFlags;
-
-typedef enum {
-  PES_TRICK_MODE_FAST_FORWARD   = 0x000,
-  PES_TRICK_MODE_SLOW_MOTION    = 0x001,
-  PES_TRICK_MODE_FREEZE_FRAME   = 0x010,
-  PES_TRICK_MODE_FAST_REVERSE   = 0x011,
-  PES_TRICK_MODE_SLOW_REVERSE   = 0x100,
-  /* ... */
-  PES_TRICK_MODE_INVALID        = 0xfff /* Not present or invalid */
-} PESTrickModeControl;
-
-typedef enum {
-  PES_FIELD_ID_TOP_ONLY         = 0x00, /* Display from top field only */
-  PES_FIELD_ID_BOTTOM_ONLY      = 0x01, /* Display from bottom field only */
-  PES_FIELD_ID_COMPLETE_FRAME   = 0x10, /* Display complete frame */
-  PES_FIELD_ID_INVALID          = 0x11  /* Reserved/Invalid */
-} PESFieldID;
-
 typedef enum {
   PES_PARSING_OK        = 0,    /* Header fully parsed and valid */
   PES_PARSING_BAD       = 1,    /* Header invalid (CRC error for ex) */
   PES_PARSING_NEED_MORE = 2     /* Not enough data to parse header */
 } PESParsingResult;
-
-typedef struct {
-  guint8        stream_id;      /* See ID_* above */
-  guint32       packet_length;  /* The size of the PES header and PES data
-                                 * (if 0 => unbounded packet) */
-  guint16       header_size;    /* The complete size of the PES header */
-
-  /* All remaining entries in this structure are optional */
-  guint8        scrambling_control; /* 0x00  : Not scrambled/unspecified,
-                                     * The following are according to ETSI TS 101 154
-                                     * 0x01  : reserved for future DVB use
-                                     * 0x10  : PES packet scrambled with Even key
-                                     * 0x11  : PES packet scrambled with Odd key
-                                     */
-  PESHeaderFlags flags;
-
-  guint64       PTS;            /* PTS (-1 if not present or invalid) */
-  guint64       DTS;            /* DTS (-1 if not present or invalid) */
-  guint64       ESCR;           /* ESCR (-1 if not present or invalid) */
-
-  guint32       ES_rate;        /* in bytes/seconds (0 if not present or invalid) */
-  PESTrickModeControl   trick_mode;
-  
-  /* Only valid for _FAST_FORWARD, _FAST_REVERSE and _FREEZE_FRAME */
-  PESFieldID    field_id;
-  /* Only valid for _FAST_FORWARD and _FAST_REVERSE */
-  gboolean      intra_slice_refresh;
-  guint8        frequency_truncation;
-  /* Only valid for _SLOW_FORWARD and _SLOW_REVERSE */
-  guint8        rep_cntrl;
-
-  guint8        additional_copy_info; /* Private data */
-  guint16       previous_PES_packet_CRC;
-
-  /* Extension fields */
-  const guint8* private_data;                   /* PES_private_data, 16 bytes long */
-  guint8        pack_header_size;               /* Size of pack_header in bytes */
-  const guint8* pack_header;
-  gint8         program_packet_sequence_counter; /* -1 if not present or invalid */
-  gboolean      MPEG1_MPEG2_identifier;
-  guint8        original_stuff_length;
-
-  guint32       P_STD_buffer_size; /* P-STD buffer size in bytes (0 if invalid
-                                    * or not present */
-
-  guint8        stream_id_extension; /* Public range (0x00 - 0x3f) only valid if stream_id == ID_EXTENDED_STREAM_ID
-                                      * Private range (0x40 - 0xff) can be present in any stream type */
-
-  gsize         extension_field_length;   /* Length of remaining extension field data */
-  const guint8* stream_id_extension_data; /* Valid if extension_field_length != 0 */
-} PESHeader;
 
 /* MPEG_TO_GST calculation requires at least 17 extra bits (100000)
  * Since maximum PTS/DTS value is coded with 33bits, we are
