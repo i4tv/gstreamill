5d768f2a2a3935270dfa63343c3f6eee3188a063
Zhang Ping <dqzhangp@163.com>
2015-11-07 18:10:04 +0100

    unref job at clean list

--- src/gstreamill.c
+++ src/gstreamill.c
@@ -1189,7 +1189,6 @@ gchar * gstreamill_job_stop (Gstreamill *gstreamill, gchar *name)
     job = get_job (gstreamill, name);
     if (job != NULL) {
         job_stop (job, SIGTERM);
-        g_object_unref (job);
         return g_strdup_printf ("{\n    \"name\": \"%s\",\n    \"result\": \"success\"\n}", name);
 
     } else {
