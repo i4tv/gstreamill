cb2f3ca80df67f6e0fb728de71bcf3721b7e8abb
张平 <zhang_ping@chinatelecom.cn>
2018-02-08 00:16:58 +0800

    warning wrong segment size

--- src/source.c
+++ src/source.c
@@ -778,11 +778,13 @@ static GstFlowReturn new_sample_callback (GstAppSink *elt, gpointer user_data)
             (stream->state->last_heartbeat - GST_SECOND > stream->next_segment_timestamp)) {
             if (!GST_BUFFER_FLAG_IS_SET (buffer, GST_BUFFER_FLAG_DELTA_UNIT) ||
                 GST_BUFFER_DURATION_IS_VALID (buffer)) {
-                GST_INFO ("duration: %ld, timestamp: %ld, last timestamp: %ld, segment_duration: %ld",
-                    stream->current_segment_duration,
-                    stream->next_segment_timestamp,
-                    stream->state->last_heartbeat,
-                    stream->current_segment_duration);
+                if (stream->current_segment_duration != stream->segment_duration) {
+                    GST_WARNING ("duration != segment_duration(%ld != %ld), timestamp: %ld, last timestamp: %ld",
+                        stream->current_segment_duration,
+                        stream->segment_duration,
+                        stream->next_segment_timestamp,
+                        stream->state->last_heartbeat);
+                }
                 ring_buffer->is_rap = TRUE;
                 ring_buffer->timestamp = stream->next_segment_timestamp;
                 ring_buffer->duration = stream->current_segment_duration;
